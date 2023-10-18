8d8a7563e331cc7ffcef5f2ebb82234eca15fa60
beatrizsouzza <114078566+beatrizsouzza@users.noreply.github.com>
2023-10-18 12:42:16 -0300

    18/10

---
+++ volume.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form")
+const resp = document.querySelector("#outResp")
+const resp1 = document.querySelector("#outResp1")
+
+
+//ouvinte
+form = addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const ladoA = Number(form.inLadoA.value)
+    const ladoB = Number(form.inLadoB.value)
+    const ladoC = Number(form.inLadoC.value)
+
+    if((ladoA > ladoB + ladoC) || (ladoB > ladoA + ladoC) || (ladoC > ladoA + ladoB)){
+        alert("Essas medidas não podem formar um triângulo . Um dos lados não pode ser maior que a soma dos outros dois.")
+        form.inLadoA.focus()
+        form.inLadoB.focus()
+        form.inLadoC.focus()
+        return 
+    }
+
+resp.innerTetx = 'Lados podem formar um triângulo'
+if(ladoA == ladoB && ladoA == ladoC){
+    resp1.innerTetx = 'Tipo: Equilátero'
+    
+}
