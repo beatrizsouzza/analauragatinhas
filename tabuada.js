c59007c06db087294f9124b3525447618cc07a24
beatrizsouzza <114078566+beatrizsouzza@users.noreply.github.com>
2023-10-25 12:13:08 -0300

    25/10

--- tabuada.js
+++ tabuada.js
@@ -5,11 +5,11 @@ const resp = document.querySelector("pre")
     form.addEventListener("submit", (e) =>{
         e.preventDefault()
 
-        const numero = Number(frm.inNumero.value)
+        const numero = Number(form.inNumero.value)
         let resposta = ""   //variável do tipo String, para concatenar a resposta
         //cria um laço de repetição 
         for(let i = 1; i <= 10; i++){
-            resposta = resposta  + numero + " x " + i + " = " + (numero = i) + "\n"
+            resposta = resposta  + numero + " x " + i + " = " + (numero * i) + "\n"
         }
         resp.innerText = resposta
 
