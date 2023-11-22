0252055bad826c073b2e889fc283338488b0dd51
beatrizsouzza <114078566+beatrizsouzza@users.noreply.github.com>
2023-11-22 12:39:04 -0300

    22/11

---
+++ farmacia.js
@@ -0,0 +1,27 @@
+const form= document.querySelector("form")
+const resp1= document.querySelector("#outResp1")
+const resp2= document=querySelector("#outResp2")
+
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const valor = Number (frm.inValor.value)
+        resp1.innerText = `Valor da compra: R$ ${valor.toFixed(2)}`
+
+        let valParcelas
+        let numParcelas = Math.floor(valor / 20)
+
+        if(numParceas > 6){
+            valParcelas = valor / 6
+            resp2.innerText =  `Pode pagarem: 6 x de: R$ ${valParcelas.toFixed(2)}`
+            else if(numParcelas <=1)
+    
+        } else if(numParcelas <= 1){
+
+        } else{
+            valParcelas = valor/numParcelas
+            resp2.innerText = ´Pode pagar em: ${numParcelas} x de R$ ${valParcelas.toFixed(2)} `
+        }
+    })
+
+
